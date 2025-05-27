c59087ec806959c796f8c54d440df7be56826f30
avitech-312 <avi.k.singh312@gmail.com>
2025-05-27 23:09:45 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,36 @@
-import fs from 'fs';
-import path from 'path';
-import Link from 'next/link';
-import matter from 'gray-matter';
-
-export async function getStaticProps() {
-  const files = fs.readdirSync(path.join(process.cwd(), 'posts'));
-
-  const posts = files.map(filename => {
-    const filePath = path.join(process.cwd(), 'posts', filename);
-    const fileContents = fs.readFileSync(filePath, 'utf8');
-    const { data } = matter(fileContents);
-
-    return {
-      slug: filename.replace('.md', ''),
-      frontMatter: data,
-    };
-  });
-
-  return {
-    props: {
-      posts,
-    },
-  };
-}
+import fs from 'fs'
+import path from 'path'
+import Link from 'next/link'
+import matter from 'gray-matter'
 
 export default function Home({ posts }) {
   return (
     <div>
-      <h1>Welcome to AI Hustle Hub</h1>
+      <h1>AI Hustle Hub Blog</h1>
       <ul>
-        {posts.map(({ slug, frontMatter }) => (
+        {posts.map(({ slug, frontmatter }) => (
           <li key={slug}>
             <Link href={`/posts/${slug}`}>
-              <a>{frontMatter.title}</a>
+              <a>{frontmatter.title}</a>
             </Link>
-            <p>{frontMatter.date}</p>
+            <p>{frontmatter.date}</p>
           </li>
         ))}
       </ul>
     </div>
-  );
+  )
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join(process.cwd(), 'posts'))
+  const posts = files.map(filename => {
+    const markdownWithMeta = fs.readFileSync(path.join(process.cwd(), 'posts', filename), 'utf-8')
+    const { data: frontmatter } = matter(markdownWithMeta)
+    return {
+      slug: filename.replace('.md', ''),
+      frontmatter,
+    }
+  })
+  return { props: { posts } }
 }
 
