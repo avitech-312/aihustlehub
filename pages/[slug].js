2a3166b97606eeb7edb85de58c8cd2ac0b47a01f
avitech-312 <avi.k.singh312@gmail.com>
2025-05-31 15:48:10 +0530

    Create [slug].js

---
+++ pages/[slug].js
@@ -0,0 +1,28 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import ReactMarkdown from 'react-markdown'
+
+export default function Post({ frontmatter, content }) {
+  return (
+    <main>
+      <h1>{frontmatter.title}</h1>
+      <p>{frontmatter.date}</p>
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </main>
+  )
+}
+
+export async function getStaticPaths() {
+  const files = fs.readdirSync(path.join(process.cwd(), 'posts'))
+  const paths = files.map(filename => ({
+    params: { slug: filename.replace('.md', '') }
+  }))
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const markdownWithMeta = fs.readFileSync(path.join(process.cwd(), 'posts', slug + '.md'), 'utf-8')
+  const { data: frontmatter, content } = matter(markdownWithMeta)
+  return { props: { frontmatter, content } }
+}
